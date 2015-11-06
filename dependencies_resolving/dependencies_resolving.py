73da91733cf53e9aed4a90acd854b30aeb13b1ba
NikolaTsolov <vikingnik@abv.bg>
2015-11-06 11:36:53 +0200

    Fixing theinstallation

--- dependencies_resolving/dependencies_resolving.py
+++ dependencies_resolving/dependencies_resolving.py
@@ -9,20 +9,8 @@ def dependencies_resolving():
     dependanceies = read_json(sys.argv[1])
     all_packages = read_json(ALL_PACKAGES)
     for dependance in dependanceies["dependencies"]:
-        dfs(dependance, all_packages)
-
-def find_all_dependances(dependance, all_packages):
-    if os.path.isfile("installed_modules/{}".format(dependance)):
-            print("{} is already installed".format(dependance))
-            return True
-    else:
-        print("Installing {}".format(dependance))
-        if len(all_packages[dependance]) == 0:
-            pass
-        else:
-            print("In order to istall {}, we need {}".format(dependance, all_packages[dependance]))
-            for item in all_packages[dependance]:
-                return (dfs(item, all_packages))
+        find_all_dependances(dependance, all_packages)
+    print("All done")
 
 def combine(string, arrey):
     string += arrey[0]
@@ -34,31 +22,45 @@ def install(filename):
     with open(filename, "w") as f:
         pass
 
-def dfs(start, all_packages):
-        visited = set()
-        stack = []
-        stack.append((start, 0))
+VISITED = set()
 
-        while len(stack) != 0:
-            current_data = stack.pop()
-            current_node = current_data[0]
-            current_level = current_data[1]
-            if os.path.isfile("installed_modules/{}".format(current_node)):
-                print("{} is already installed".format(current_node))
-                visited.add(current_node)
-            if current_node not in visited:
-                visited.add(current_node)
-                print("Installing {}".format(current_node))
-                install("installed_modules/{}".format(current_node))
-                if len(all_packages[current_node]) != 0:
-                    text = "In order to istall {}, we need ".format(current_node)
-                    print(combine(text, all_packages[current_node]))
-                for neighbour in all_packages[current_node]:
-                    stack.append((neighbour, current_level + 1))
+def find_all_dependances(start, all_packages):
+    if os.path.isfile("installed_modules/{}".format(start)):
+        print("{} is already installed".format(start))
+        VISITED.add(start)
+    if start not in VISITED:
+        VISITED.add(start)
+        print("Installing {}".format(start))
+        if len(all_packages[start]) != 0:
+            text = "In order to istall {}, we need ".format(start)
+            print(combine(text, all_packages[start]))
+        for neighbour in all_packages[start]:
+            find_all_dependances(neighbour, all_packages)
+    install("installed_modules/{}".format(start))
+    print("Installation of {} has finished".format(start))
+    return True
 
-        print("All done")
+#def dfs(start, all_packages):
+#        visited = set()
+#        stack = []
+#        stack.append((start, 0))
+#
+#        while len(stack) != 0:
+#            current_data = stack.pop()
+#            current_node = current_data[0]
+#            if os.path.isfile("installed_modules/{}".format(current_node)):
+#                print("{} is already installed".format(current_node))
+#                visited.add(current_node)
+#            if current_node not in visited:
+#                visited.add(current_node)
+#                print("Installing {}".format(current_node))
+#                install("installed_modules/{}".format(current_node))
+#                if len(all_packages[current_node]) != 0:
+#                    text = "In order to istall {}, we need ".format(current_node)
+#                    print(combine(text, all_packages[current_node]))
+#                for neighbour in all_packages[current_node]:
+#                    stack.append(neighbour)
 
-        return True
 
 
 
